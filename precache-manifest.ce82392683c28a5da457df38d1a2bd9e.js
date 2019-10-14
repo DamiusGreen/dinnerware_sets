117253bcf7029a90f7bf4fee0a8d257cce33d36a
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 13:08:05 -0400

    Updates

---
+++ precache-manifest.ce82392683c28a5da457df38d1a2bd9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1552f935df9e7a1837fc798e2fb271e1",
+    "url": "/dinnerware_sets/index.html"
+  },
+  {
+    "revision": "535be3e021b4b09fb70b",
+    "url": "/dinnerware_sets/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/dinnerware_sets/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "535be3e021b4b09fb70b",
+    "url": "/dinnerware_sets/static/js/main.40117f1f.chunk.js"
+  },
+  {
+    "revision": "abba6347379de136c7cc",
+    "url": "/dinnerware_sets/static/js/runtime~main.67eb189f.js"
+  }
+]);
